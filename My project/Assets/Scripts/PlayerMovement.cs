e9a9add85cfe49ab2f09e1d70e2184152cf06a79
Leo P <leo.projceski@education.nsw.gov.au>
2024-05-16 11:52:15 +1000

    trying to fix code

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8f;
+    private float spped = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
@@ -36,7 +36,7 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
-    private bool IsGrounded()
+    private bool IsGround()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
