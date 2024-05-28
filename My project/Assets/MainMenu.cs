776c378279d583e637f4ce17ea2554ea54ffcf13
Leo P <leo.projceski@education.nsw.gov.au>
2024-05-28 11:54:26 +1000

    made button work

--- My project/Assets/MainMenu.cs
+++ My project/Assets/MainMenu.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Playgame()
     {
-        
+        SceneManager.LoadSceneAsync(1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
