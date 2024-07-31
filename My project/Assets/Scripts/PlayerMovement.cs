24298fcb4edbcfcca7b81aee016be38cba8b2c05
Leo P <leo.projceski@education.nsw.gov.au>
2024-07-31 14:12:37 +1000

    fixed jump

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -16,11 +16,12 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Update()
     {
-        Debug.Log(IsGrounded);
+        
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump") && IsGrounded())  //&& IsGrounded()
         {
+            //Debug.Log(Input.GetButtonDown("Jump"));
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
         
@@ -37,8 +38,10 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
+
     private bool IsGrounded()
     {
+        Debug.Log(Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer));
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
